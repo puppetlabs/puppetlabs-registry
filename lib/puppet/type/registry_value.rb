8601e9250cb747d88b69545c04fbf56d843343be
Josh Cooper <josh@puppetlabs.com>
2012-04-18 13:29:44 -0700

    Remove debugging

--- lib/puppet/type/registry_value.rb
+++ lib/puppet/type/registry_value.rb
@@ -26,74 +26,12 @@ Puppet::Type.newtype(:registry_value) do
 
   newproperty(:type) do
     newvalues(:string, :array, :dword, :qword, :binary, :expand)
-
-    def insync?(currentvalue)
-      require 'ruby-debug'; debugger
-      super(currentvalue)
-    end
-
-    def property_matches?(current, desired)
-      require 'ruby-debug'; debugger
-      super(current, desired)
-    end
-
-    # munge do |value|
-    #   require 'ruby-debug'; debugger
-    #   resource.name2type(value.intern)
-    # end
-
-    # def is_to_s(currentvalue)
-    #   type2name(currentvalue).to_s || currentvalue
-    # end
-
-    # def should_to_s(newvalue)
-    #   type2name(newvalue).to_s || newvalue
-    # end
-
     defaultto :string
   end
 
   newproperty(:data) do
     desc "The data of the registry value."
     defaultto ''
-
-    # munge do |value|
-    #   case resource[:type]
-    #   when :string, :expand
-    #     value
-    #   when :dword, :qword
-    #     require 'ruby-debug'; debugger
-    #     Integer(value)
-    #   when :binary
-    #     value
-    #   when :array
-    #     value
-    #   end
-    # end
-
-    # unmunge do |value|
-    #   case resource[:type]
-    #   when :string, :expand
-    #     value
-    #   when :dword, :qword
-    #     require 'ruby-debug'; debugger
-    #     value.to_s
-    #   when :binary
-    #     value
-    #   when :array
-    #     value
-    #   end
-    # end
-
-    def insync?(currentvalue)
-      require 'ruby-debug'; debugger
-      super(currentvalue)
-    end
-
-    def property_matches?(current, desired)
-      require 'ruby-debug'; debugger
-      super(current, desired)
-    end
   end
 
   validate do
