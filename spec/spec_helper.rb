51ff8c4959acbcc020062776ee4534e3f500afd0
Thomas Linkin <trlinkin@gmail.com>
2013-08-01 11:17:09 -0400

    Update spec_helper for rspec 2.x
    Without this patch the spec_helper.rb does not load things correctly to
    run these tests with rspec 2.x and mocha.
    
    This patch changes the require statements to load 'rspec' and
    'rspec/expectations' correctly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH.unshift File.join(dir, 'lib')
 
-require 'mocha'
 require 'puppet'
-require 'rspec'
+gem 'rspec', '>=2.0.0'
+require 'rspec/expectations'
 
 RSpec.configure do |config|
     config.mock_with :mocha
