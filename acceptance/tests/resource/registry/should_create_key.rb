797d5f194f1ac7adaf7524a1b4d86e70dbee0854
Travis Fields <travis@puppetlabs.com>
2014-08-21 16:05:52 -0700

    Fix registry testing, remove dependency of master to enable both foss and pe testing

--- acceptance/tests/resource/registry/should_create_key.rb
+++ acceptance/tests/resource/registry/should_create_key.rb
@@ -17,9 +17,16 @@ master_manifest_content = <<HERE
 notify { fact_phase: message => "fact_phase: $fact_phase" }
 
 registry_key { 'HKLM\\Software\\Vendor': ensure => present }
+case $fact_phase {
+      '2': { include phase2 }
+      '3': { include phase3 }
+  default: { include phase1 }
+}
+HERE
 
 # Setup keys to purge
-class phase1 {
+phase1 = <<PHASE1
+  registry_key { 'HKLM\\Software\\Vendor': ensure => present }
   Registry_key { ensure => present }
   Registry_value { ensure => present, data => 'Puppet Default Data' }
 
@@ -42,20 +49,22 @@ class phase1 {
     registry_value { '32:#{keypath}\\SubKeyToPurge\\Value2': }
     registry_value { '32:#{keypath}\\SubKeyToPurge\\Value3': }
   }
-}
+PHASE1
 
 # Purge the keys in a subsequent run
-class phase2 {
+phase2 = <<PHASE2
+  registry_key { 'HKLM\\Software\\Vendor': ensure => present }
   Registry_key { ensure => present, purge_values => true }
 
   registry_key { '#{keypath}\\SubKeyToPurge': }
   if $architecture == 'x64' {
     registry_key { '32:#{keypath}\\SubKeyToPurge': }
   }
-}
+PHASE2
 
 # Delete our keys
-class phase3 {
+phase3 = <<PHASE3
+  registry_key { 'HKLM\\Software\\Vendor': ensure => present }
   Registry_key { ensure => absent }
 
   # These have relationships because autorequire break things when
@@ -75,88 +84,78 @@ class phase3 {
       require => Registry_key['32:#{keypath}\\SubKeyToPurge', '32:#{keypath}\\SubKey1'],
     }
   }
-}
+PHASE3
 
-case $fact_phase {
-      '2': { include phase2 }
-      '3': { include phase3 }
-  default: { include phase1 }
-}
-HERE
 
 # Setup the master to use the modules specified in the --modules option
 
 
-step "Start the master" do
-  setup_master master_manifest_content
-  with_puppet_running_on master, :__commandline_args__ => master_options do
-    # A set of keys we expect Puppet to create
-    keys_created_native = [
+step "Start should_create_key test" do
+  # setup_master master_manifest_content
+  # with_puppet_running_on master, :__commandline_args__ => master_options do
+  # A set of keys we expect Puppet to create
+  keys_created_native = [
       /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\].ensure: created/,
       /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\].ensure: created/
-    ]
+  ]
 
-    keys_created_wow = [
-     /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\].ensure: created/,
-     /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\].ensure: created/
-    ]
+  keys_created_wow = [
+      /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\].ensure: created/,
+      /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\].ensure: created/
+  ]
 
-    # A set of regular expression of values to be purged in phase 2.
-    values_purged_native = [
+  # A set of regular expression of values to be purged in phase 2.
+  values_purged_native = [
       /Registry_value\[hklm.Software.Vendor.PuppetLabsTest\w+.SubKeyToPurge.Value1\].ensure: removed/,
       /Registry_value\[hklm.Software.Vendor.PuppetLabsTest\w+.SubKeyToPurge.Value2\].ensure: removed/,
       /Registry_value\[hklm.Software.Vendor.PuppetLabsTest\w+.SubKeyToPurge.Value3\].ensure: removed/
-    ]
+  ]
 
-    values_purged_wow = [
+  values_purged_wow = [
       /Registry_value\[32:hklm.Software.Vendor.PuppetLabsTest\w+.SubKeyToPurge.Value1\].ensure: removed/,
       /Registry_value\[32:hklm.Software.Vendor.PuppetLabsTest\w+.SubKeyToPurge.Value2\].ensure: removed/,
       /Registry_value\[32:hklm.Software.Vendor.PuppetLabsTest\w+.SubKeyToPurge.Value3\].ensure: removed/
-    ]
+  ]
 
-    windows_agents.each do |agent|
-      this_agent_args = agent_args % get_test_file_path(agent, agent_lib_dir)
-      is_x64 = x64?(agent)
+  windows_agents.each do |agent|
+    is_x64 = x64?(agent)
 
-      keys_created  = keys_created_native  + (is_x64 ? keys_created_wow : [])
-      values_purged = values_purged_native + (is_x64 ? values_purged_wow : [])
+    keys_created = keys_created_native + (is_x64 ? keys_created_wow : [])
+    values_purged = values_purged_native + (is_x64 ? values_purged_wow : [])
 
-      # Do the first run and make sure the key gets created.
-      step "Registry - Phase 1.a - Create some keys"
-      run_agent_on(agent, this_agent_args, :acceptable_exit_codes => agent_exit_codes) do
-        keys_created.each do |key_re|
-          assert_match(key_re, result.stdout,
-                       "Expected #{key_re.inspect} to match the output. (First Run)")
-        end
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    # Do the first run and make sure the key gets created.
+    step "Registry - Phase 1.a - Create some keys"
+    apply_manifest_on(agent, phase1, :acceptable_exit_codes => agent_exit_codes) do
+      keys_created.each do |key_re|
+        assert_match(key_re, result.stdout,
+                     "Expected #{key_re.inspect} to match the output. (First Run)")
       end
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    end
 
-      step "Registry - Phase 1.b - Make sure Puppet is idempotent"
-      # Do a second run and make sure the key isn't created a second time.
-      run_agent_on(agent, this_agent_args, :acceptable_exit_codes => agent_exit_codes) do
-        keys_created.each do |key_re|
-          assert_no_match(key_re, result.stdout,
-                       "Expected #{key_re.inspect} NOT to match the output. (First Run)")
-        end
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    step "Registry - Phase 1.b - Make sure Puppet is idempotent"
+    # Do a second run and make sure the key isn't created a second time.
+    apply_manifest_on(agent, phase1, :acceptable_exit_codes => agent_exit_codes) do
+      keys_created.each do |key_re|
+        assert_no_match(key_re, result.stdout,
+                        "Expected #{key_re.inspect} NOT to match the output. (First Run)")
       end
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    end
 
-      step "Registry - Phase 2 - Make sure purge_values works"
-      on agent, puppet_agent(this_agent_args, :environment => { 'FACTER_FACT_PHASE' => '2' }),
-        :acceptable_exit_codes => agent_exit_codes do
-        values_purged.each do |val_re|
-          assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
-        end
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    step "Registry - Phase 2 - Make sure purge_values works"
+    apply_manifest_on(agent, phase2, :acceptable_exit_codes => agent_exit_codes, :environment => {'FACTER_FACT_PHASE' => '2'}) do
+      values_purged.each do |val_re|
+        assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
       end
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    end
 
-      step "Registry - Phase 3 - Clean up"
-      on agent, puppet_agent(this_agent_args, :environment => { 'FACTER_FACT_PHASE' => '3' }),
-        :acceptable_exit_codes => agent_exit_codes do
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
-      end
+    step "Registry - Phase 3 - Clean up"
+    apply_manifest_on(agent, phase3, :acceptable_exit_codes => agent_exit_codes, :environment => {'FACTER_FACT_PHASE' => '3'}) do
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
     end
   end
 end
 
-clean_up
+# clean_up
--- acceptance/tests/resource/registry/should_manage_values.rb
+++ acceptance/tests/resource/registry/should_manage_values.rb
@@ -13,15 +13,12 @@ keyname = "PuppetLabsTest_Value_#{randomstring(8)}"
 vendor_path = "HKLM\\Software\\Vendor"
 keypath = "#{vendor_path}\\#{keyname}"
 
-master_manifest_content = <<HERE
-notify { fact_phase: message => "fact_phase: $fact_phase" }
-
-registry_key { '#{vendor_path}': ensure => present }
-if $architecture == 'x64' {
-  registry_key { '32:#{vendor_path}': ensure => present }
-}
-
-class phase1 {
+phase1 = <<P1
+  notify { fact_phase: message => "fact_phase: $fact_phase" }
+  registry_key { '#{vendor_path}': ensure => present }
+  if $architecture == 'x64' {
+    registry_key { '32:#{vendor_path}': ensure => present }
+  }
   Registry_key { ensure => present }
   registry_key { '#{keypath}': }
   registry_key { '#{keypath}\\SubKey1': }
@@ -200,138 +197,126 @@ class phase1 {
       data => "DEAD BEEF CAF${::fact_phase}"
     }
   }
+P1
 
-}
 
-# We always include phase1 because the resources will contain ${::fact_phase}
-# which will trigger resource changed event messages we can test against if we
-# run "phase 2" twice after phase1 has already been run twice.
-case $fact_phase {
-  default: { include phase1 }
-}
-HERE
+step "Start testing should_manage_values" do
 
-# Setup the master to use the modules specified in the --modules option
-setup_master master_manifest_content
+  x64 = x64?(default)
 
-step "Start the master" do
-  with_puppet_running_on master, :__commandline_args__ => master_options do
-    windows_agents.each do |agent|
-      this_agent_args = agent_args % get_test_file_path(agent, agent_lib_dir)
-      x64 = x64?(agent)
+  # A set of keys we expect Puppet to create
+  phase1_resources_created = [
+      /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\].ensure: created/,
+      /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\].ensure: created/,
+      /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey2\].ensure: created/,
+  ]
 
-      # A set of keys we expect Puppet to create
-      phase1_resources_created = [
-        /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\].ensure: created/,
-        /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\].ensure: created/,
-        /Registry_key\[HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey2\].ensure: created/,
-      ]
+  if x64?(agent)
+    phase1_resources_created += [
+        /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\].ensure: created/,
+        /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\].ensure: created/,
+        /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey2\].ensure: created/,
+    ]
+  end
 
-      if x64?(agent)
-        phase1_resources_created += [
-          /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\].ensure: created/,
-          /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\].ensure: created/,
-          /Registry_key\[32:HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey2\].ensure: created/,
-        ]
+  # A set of values we expect Puppet to change in Phase 2
+  phase2_resources_changed = Array.new
+
+  prefixes = ['']
+  prefixes << '32:' if x64
+
+  # This is just to save a whole bunch of copy / paste
+  prefixes.each do |prefix|
+    # We should have created 4 REG_SZ values
+    1.upto(4).each do |idx|
+      phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueString#{idx}\].ensure: created/
+      phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueString#{idx}\].data: data changed 'Should be a string phase=1' to 'Should be a string phase=2'/
+    end
+    # We should have created 5 REG_MULTI_SZ values
+    1.upto(5).each do |idx|
+      phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueArray#{idx}\].ensure: created/
+    end
+
+    # The first two array items are an exception
+    1.upto(2).each do |idx|
+      phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueArray#{idx}\].data: data changed 'Should be an array L1 phase=1' to 'Should be an array L1 phase=2'/
+    end
+
+    # The rest of the array items are OK and have 2 "lines" each.
+    3.upto(5).each do |idx|
+      phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueArray#{idx}\].data: data changed 'Should be an array L1 phase=1,Should be an array L2 phase=1' to 'Should be an array L1 phase=2,Should be an array L2 phase=2'/
+    end
+
+    # We should have created 2 REG_EXPAND_SZ values
+    1.upto(2).each do |idx|
+      phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueExpand#{idx}\].ensure: created/
+      phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueExpand#{idx}\].data: data changed '%SystemRoot% - Should be a REG_EXPAND_SZ phase=1' to '%SystemRoot% - Should be a REG_EXPAND_SZ phase=2'/
+    end
+    # We should have created 1 qword
+    1.upto(1).each do |idx|
+      phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueQword#{idx}\].ensure: created/
+      phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueQword#{idx}\].data: data changed '1' to '2'/
+    end
+    # We should have created 1 dword
+    1.upto(1).each do |idx|
+      phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueDword#{idx}\].ensure: created/
+      phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueDword#{idx}\].data: data changed '1' to '2'/
+    end
+    # We should have created 2 binary values
+    1.upto(2).each do |idx|
+      phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueBinary#{idx}\].ensure: created/
+    end
+    # We have different data for the binary values
+    phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueBinary1\].data: data changed '01' to '02'/
+    phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueBinary2\].data: data changed 'de ad be ef ca f1' to 'de ad be ef ca f2'/
+  end
+
+  agents.each do |agent|
+    step "Registry Values - Phase 1.a - Create some values"
+    apply_manifest_on agent,  phase1, :environment => {'FACTER_FACT_PHASE' => '1'}, :acceptable_exit_codes => agent_exit_codes do
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+      phase1_resources_created.each do |val_re|
+        assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
       end
+    end
 
-      # A set of values we expect Puppet to change in Phase 2
-      phase2_resources_changed = Array.new
-
-      prefixes = ['']
-      prefixes << '32:' if x64
-
-      # This is just to save a whole bunch of copy / paste
-      prefixes.each do |prefix|
-        # We should have created 4 REG_SZ values
-        1.upto(4).each do |idx|
-          phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueString#{idx}\].ensure: created/
-          phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueString#{idx}\].data: data changed 'Should be a string phase=1' to 'Should be a string phase=2'/
-        end
-        # We should have created 5 REG_MULTI_SZ values
-        1.upto(5).each do |idx|
-          phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueArray#{idx}\].ensure: created/
-        end
-
-        # The first two array items are an exception
-        1.upto(2).each do |idx|
-          phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueArray#{idx}\].data: data changed 'Should be an array L1 phase=1' to 'Should be an array L1 phase=2'/
-        end
-
-        # The rest of the array items are OK and have 2 "lines" each.
-        3.upto(5).each do |idx|
-          phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueArray#{idx}\].data: data changed 'Should be an array L1 phase=1,Should be an array L2 phase=1' to 'Should be an array L1 phase=2,Should be an array L2 phase=2'/
-        end
-
-        # We should have created 2 REG_EXPAND_SZ values
-        1.upto(2).each do |idx|
-          phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueExpand#{idx}\].ensure: created/
-          phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueExpand#{idx}\].data: data changed '%SystemRoot% - Should be a REG_EXPAND_SZ phase=1' to '%SystemRoot% - Should be a REG_EXPAND_SZ phase=2'/
-        end
-        # We should have created 1 qword
-        1.upto(1).each do |idx|
-          phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueQword#{idx}\].ensure: created/
-          phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueQword#{idx}\].data: data changed '1' to '2'/
-        end
-        # We should have created 1 dword
-        1.upto(1).each do |idx|
-          phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueDword#{idx}\].ensure: created/
-          phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueDword#{idx}\].data: data changed '1' to '2'/
-        end
-        # We should have created 2 binary values
-        1.upto(2).each do |idx|
-          phase1_resources_created << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueBinary#{idx}\].ensure: created/
-        end
-        # We have different data for the binary values
-        phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueBinary1\].data: data changed '01' to '02'/
-        phase2_resources_changed << /Registry_value\[#{prefix}HKLM.Software.Vendor.PuppetLabsTest\w+\\SubKey1\\ValueBinary2\].data: data changed 'de ad be ef ca f1' to 'de ad be ef ca f2'/
+    step "Registry Values - Phase 1.b - Make sure Puppet is idempotent"
+    apply_manifest_on agent,  phase1, :environment => {'FACTER_FACT_PHASE' => '1'}, :acceptable_exit_codes => agent_exit_codes do
+      phase1_resources_created.each do |val_re|
+        assert_no_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
       end
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    end
 
-      step "Registry Values - Phase 1.a - Create some values"
-      on agent, puppet_agent(this_agent_args, :environment => { 'FACTER_FACT_PHASE' => '1' }), :acceptable_exit_codes => agent_exit_codes do
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
-        phase1_resources_created.each do |val_re|
-          assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
-        end
+    step "Registry Values - Phase 2.a - Change some values"
+    apply_manifest_on agent,  phase1, :environment => {'FACTER_FACT_PHASE' => '2'}, :acceptable_exit_codes => agent_exit_codes do
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+      phase2_resources_changed.each do |val_re|
+        assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
       end
+    end
 
-      step "Registry Values - Phase 1.b - Make sure Puppet is idempotent"
-      on agent, puppet_agent(this_agent_args, :environment => { 'FACTER_FACT_PHASE' => '1' }), :acceptable_exit_codes => agent_exit_codes do
-        phase1_resources_created.each do |val_re|
-          assert_no_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
-        end
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    step "Registry Values - Phase 2.b - Make sure Puppet is idempotent"
+    apply_manifest_on agent,  phase1, :environment => {'FACTER_FACT_PHASE' => '2'}, :acceptable_exit_codes => agent_exit_codes do
+      phase2_resources_changed.each do |val_re|
+        assert_no_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
       end
+      assert_no_match(/err:/, result.stdout, "Expected no error messages.")
+    end
 
-      step "Registry Values - Phase 2.a - Change some values"
-      on agent, puppet_agent(this_agent_args, :environment => { 'FACTER_FACT_PHASE' => '2' }), :acceptable_exit_codes => agent_exit_codes do
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
-        phase2_resources_changed.each do |val_re|
-          assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
-        end
-      end
-
-      step "Registry Values - Phase 2.b - Make sure Puppet is idempotent"
-      on agent, puppet_agent(this_agent_args, :environment => { 'FACTER_FACT_PHASE' => '2' }), :acceptable_exit_codes => agent_exit_codes do
-        phase2_resources_changed.each do |val_re|
-          assert_no_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
-        end
-        assert_no_match(/err:/, result.stdout, "Expected no error messages.")
-      end
-
-      step "Registry Values - Phase 3 - Check the default value (#14572)"
-      # (#14572) This test uses the 'native' version of reg.exe to read the
-      # default value of a registry key.  It should contain the string shown in
-      # val_re.
-      dir = native_sysdir(agent)
-      if not dir
-        Log.warn("Cannot query 64-bit view of registry from 32-bit process, skipping")
-      else
-        on agent, "#{dir}/reg.exe query '#{keypath}\\Subkey1'" do
-          val_re = /\(Default\)    REG_SZ    Default Data phase=2/i
-          assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
-        end
+    step "Registry Values - Phase 3 - Check the default value (#14572)"
+    # (#14572) This test uses the 'native' version of reg.exe to read the
+    # default value of a registry key.  It should contain the string shown in
+    # val_re.
+    dir = native_sysdir(agent)
+    if not dir
+      Log.warn("Cannot query 64-bit view of registry from 32-bit process, skipping")
+    else
+      on agent, "#{dir}/reg.exe query '#{keypath}\\Subkey1'" do
+        val_re = /\(Default\)    REG_SZ    Default Data phase=2/i
+        assert_match(val_re, result.stdout, "Expected output to contain #{val_re.inspect}.")
       end
     end
   end
 end
+
