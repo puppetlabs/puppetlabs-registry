1014c498f84c152515f8efd9aa31498f60b600a3
Hunter Haugen <hunter@puppetlabs.com>
2014-08-25 17:31:07 -0700

    Release 1.0.3

--- acceptance/setup/install_puppet.rb
+++ acceptance/setup/install_puppet.rb
@@ -1,7 +1,7 @@
 test_name "Installing Puppet" do
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '../..'))
-  version = ENV['PUPPET_VERSION'] || '3.6.2-1066-g5010aba-x64'
-  download_url = ENV['WIN_DOWNLOAD_URL'] || 'http://builds.puppetlabs.lan/puppet/5010aba1b89f04788736dd49227c46617516bb92/artifacts/windows/'
+  version = ENV['PUPPET_VERSION'] || '3.6.2'
+  download_url = ENV['WIN_DOWNLOAD_URL'] || 'http://downloads.puppetlabs.com/windows/'
   hosts.each do |host|
     if host['platform'] =~ /windows/
       install_puppet_from_msi(host,
