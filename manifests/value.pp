d72706b5db4fa9f0bdf1f9cb896b116fcb243a1e
Steve Brito <stevebrito@google.com>
2017-02-07 16:36:22 -0800

    Fix lint

--- manifests/value.pp
+++ manifests/value.pp
@@ -75,7 +75,7 @@ define registry::value (
 
   # If value_real is an empty string then the default value of the key will be
   # managed.
-  registry_value { "$key\\${value_real}":
+  registry_value { "${key}\\${value_real}":
     value_name => $value_real,
     type       => $type,
     data       => $data,
