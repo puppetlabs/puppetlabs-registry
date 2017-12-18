e3206037d6444aa8c0fb3a67419c9be7e4b2c763
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-12-18 14:42:05 +0800

    (MODULES-2957) Add a name method for the registry_value type
    Previously the registry_key would fail to purge unmanaged registry values. The
    registry_key eval_generate method creates regitry_value resources, however as
    this is a compound namevar resource it "breaks" (sends nil) the name which is
    used the eval_generate method to uniquely identify resources.  Instead we just
    return the title ourselves.

--- lib/puppet/type/registry_value.rb
+++ lib/puppet/type/registry_value.rb
@@ -132,6 +132,16 @@ Puppet::Type.newtype(:registry_value) do
     end
   end
 
+  # Need to override the normal name method due to PUP-8620
+  # https://tickets.puppetlabs.com/browse/PUP-8260
+  # The registry_key eval_generate method creates regitry_value resources, however
+  # as this is a compound namevar resource it "breaks" (sends nil) the name which
+  # is used the eval_generate method to uniquely identify resources.  Instead we
+  # just return the title ourselves
+  def name
+    self.title
+  end
+
   # Autorequire the nearest ancestor registry_key found in the catalog.
   autorequire(:registry_key) do
     # This is a value path and not a key path because it's based on the path of
