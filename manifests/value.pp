puppetlabs/puppetlabs-registry

commit 48a0b07449ab7d057e97dc6c00ad94e823c6503d

(MODULES-2957) Use Puppet 4 Data Types
Previously this module used avalidation functions from stdlib which are
now deprecated due to Puppet 4 Data types.  This commit:
* Changes the validate_re functions to use Puppet 4 data types
* Removes the stdlib dependency as it is no longer required
* Adds tests for the registry::value class for parameter validation

manifests/value.pp
@@ -40,24 +40,16 @@
 #   }
 #
 define registry::value (
-  $key,
-  $value = undef,
-  $type  = 'string',
-  $data  = undef,
+  Pattern[/^\w+/]           $key,
+  Optional[String]          $value = undef,
+  Optional[Pattern[/^\w+/]] $type = 'string',
+  Optional[String]          $data  = undef,
 ) {
 
   # ensure windows os
   if $::operatingsystem != 'windows'{
     fail("Unsupported OS ${::operatingsystem}")
   }
-
-  # validate our inputs.
-  validate_re($key, '^\w+',
-    "key parameter must not be empty but it is key => '${key}'")
-  validate_re($type, '^\w+',
-    "type parameter must not be empty but it is type => '${type}'")
-
-
 
   $value_real = $value ? {
     undef       => $name,

spec/defines/value_spec.rb
@@ -1,0 +1,41 @@
+require 'spec_helper'
+
+RSpec.describe 'registry::value', :type => :define do
+  let(:title) { 'RegistryTest' }
+  let(:facts) {{
+    'operatingsystem' => 'windows'
+  }}
+
+  context 'Given a minimal resource' do
+    let(:params) {{
+      :key => 'HKLM\Software\Vendor',
+    }}
+
+    it { is_expected.to compile }
+
+    context 'On a non-windows platform' do
+      let(:facts) {{
+        'operatingsystem' => 'Debian'
+      }}
+
+      it { is_expected.to compile.and_raise_error(/Unsupported OS/) }
+    end
+  end
+
+  context 'Given an empty key' do
+    let(:params) {{
+      :key => '',
+    }}
+
+    it { is_expected.to compile.and_raise_error(/parameter 'key'/) }
+  end
+
+  context 'Given an empty type' do
+    let(:params) {{
+      :key => 'HKLM\Software\Vendor',
+      :type => '',
+    }}
+
+    it { is_expected.to compile.and_raise_error(/parameter 'type'/) }
+  end
+end

commit e36dc742ae77790192bad6172bc1ec456a53a7db

(MODULES-6528) Change registry::value types
 - Fix the bug introduced in 48a0b07449ab7d057e97dc6c00ad94e823c6503d
   that did not have lenient enough typing for the $data parameter to
   the registry::value defined type.

 - This commit adds support for numerics, which are necessary for
   dword and qword. It also adds support for arrays, which are always
   an array of strings (corresponding to REG_MULTI_SZ).

manifests/value.pp
@@ -43,7 +43,11 @@ define registry::value (
   Pattern[/^\w+/]           $key,
   Optional[String]          $value = undef,
   Optional[Pattern[/^\w+/]] $type = 'string',
-  Optional[String]          $data  = undef,
+  Optional[Variant[
+    String,
+    Numeric,
+    Array[Variant[String]
+  ]]]                       $data  = undef,
 ) {
 
   # ensure windows os