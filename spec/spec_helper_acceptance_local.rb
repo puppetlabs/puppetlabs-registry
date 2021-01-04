053b251cdf1207275fec34eace53eacf0414df23
David Schmitt <david.schmitt@puppet.com>
2021-01-04 15:11:23 +0000

    Fix frozen string error

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -3,7 +3,7 @@
 def random_string(length)
   chars = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
   str = ''
-  1.upto(length) { |_i| str << chars[rand(chars.size - 1)] }
+  1.upto(length) { |_i| str += chars[rand(chars.size - 1)] }
   str
 end
 
